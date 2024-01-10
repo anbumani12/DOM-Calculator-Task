6e3055895f79a786f0c717c134d1e46aba4c3384
anbumani12 <anbumani15012002@gmail.com>
2024-01-10 22:47:55 +0530

    code completed

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-
-let display = document.getElementById('result');
-let currentInput = '';
+let display = document.getElementById("result");
+let currentInput = "";
 
 function appendNumber(num) {
-    currentInput += num;
-    display.value = currentInput;
+  currentInput += num;
+  display.value = currentInput;
 }
 
 function setOperation(operator) {
-    currentInput += operator;
-    display.value = currentInput;
+  currentInput += operator;
+  display.value = currentInput;
 }
 
 function calculate() {
-    try {
-        currentInput = eval(currentInput);
-        display.value = currentInput;
-    } catch (error) {
-        alert('Invalid expression');
-        clearDisplay();
-    }
+  try {
+    currentInput = eval(currentInput);
+    display.value = currentInput;
+  } catch (error) {
+    alert("Invalid expression");
+    clearDisplay();
+  }
 }
 
 function clearDisplay() {
-    currentInput = '';
-    display.value = '';
+  currentInput = "";
+  display.value = "";
 }
 
-document.addEventListener('keydown', function (event) {
-    if (!isNaN(event.key)) {
-        appendNumber(event.key);
-    } else {   
-        alert('Only numbers are allowed');
-    }
+document.addEventListener("keydown", function (event) {
+  if (!isNaN(event.key)) {
+    appendNumber(event.key);
+  } else {
+    alert("Only numbers are allowed");
+  }
 });
